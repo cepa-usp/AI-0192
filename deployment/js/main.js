b90d68ff286558079b05a639b1f7884766c4fd02
Alexandre <aledrones@hotmail.com>
2015-01-27 12:17:42 -0200

    Versão em html pronta, faltando apenas ajustes visuais.

---
+++ deployment/js/main.js
@@ -0,0 +1,43 @@
+var btnPlayPause;
+var timer = 0;
+var current = 0;
+var started = false;
+var animationFrame;
+
+var t;
+
+function init(){
+	btnPlayPause = $("#start_stop_button");
+	btnPlayPause.on("mousedown", btnClick);
+	t = $(".timer");
+	window.requestAnimationFrame(updateTimer);
+}
+
+function btnClick(evt){
+	if(started){
+		started = false;
+		btnPlayPause.html("INICIAR");
+		$(".time").html(timer.toFixed(2) + " segundos");
+		if(Math.abs(timer - 15) < 0.5){
+			$(".final").html("Você acertou!");
+		}else{
+			$(".final").html("Tente novamente.");
+		}
+		$(".result").show();
+	}else{
+		timer = 0;
+		started = true;
+		btnPlayPause.html("PARAR");
+		$(".result").hide();
+	}
+}
+
+function updateTimer(timestamp){
+	var dt = (timestamp - current)/1000;
+	current = timestamp;
+
+	if(started) timer += dt;
+	t.html(timer.toFixed(2));
+
+	window.requestAnimationFrame(updateTimer);
+}
